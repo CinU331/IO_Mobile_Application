ce7d7ec5d81e1426bf9057bb114aea8e3cc18c2f
CinU <cinu331@gmail.com>
2018-12-14 15:02:16 +0100

    Initial commit, added activities.

---
+++ VotingSystem/VotingSystem.Android/Authentication.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace VotingSystem.Droid
+{
+    [Activity(Label = "Authentication")]
+    public class Authentication : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        private string login;
+        private string password;
+        private bool beAbleToVote;
+        private string token;
+
+        public void TryToLogIn(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        public void Permissions()
+        {
+            beAbleToVote = true;
+            token = "1";
+        }
+
+        public void GoToVoting(bool beAbleToVote)
+        {
+            // if (beAbleToVote)
+        }
+
+        public void GoToResults()
+        {
+
+        }
+
+        public string GetLogin()
+        {
+            return login;
+        }
+        public string GetToken()
+        {
+            return token;
+        }
+    }
+}
---
+++ VotingSystem/VotingSystem.Android/Vote.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+
+namespace VotingSystem.Droid
+{
+    [Activity(Label = "Vote")]
+    public class Vote : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        // Create your application here
+        private List<string> candidates;
+        private Authentication authentication;
+
+        public List<string> GetCandidatesList()
+        {
+            return candidates;
+        }
+
+        public bool CheckIfVoiceHasAlreadyBeenGiven(string user)
+        {
+            return false;
+        }
+
+        public bool SaveVote(string candidat, string user)
+        {
+            return true;
+        }
+
+        public void DisplayMessage()
+        {
+
+        }
+
+        public void GoToResults()
+        {
+
+        }
+    }
+}
