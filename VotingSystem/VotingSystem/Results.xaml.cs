25b56c88907aa0e90dea37c52de887b3878f5273
CinU <cinu331@gmail.com>
2018-12-18 22:36:25 +0100

    Added choice of voting when you move to Results or Vote.

--- VotingSystem/VotingSystem/Results.xaml.cs
+++ VotingSystem/VotingSystem/Results.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VotingSystem
@@ -8,6 +10,7 @@ namespace VotingSystem
 	{
         private string attendanceImagePath;
         private string distributionOfVotesImagePath;
+        private Picker picker;
         private Label attendance;
         private Label distributionOfVotes;
         private Image attendanceImage;
@@ -15,13 +18,44 @@ namespace VotingSystem
 
 		public Results ()
 		{
-			InitializeComponent ();
-            attendanceImagePath = "attendance.jpg";
-            GetAttendance(attendanceImagePath);
-            distributionOfVotesImagePath = "distributionOfVotes.png";
-            GetDistributionOfVotes(distributionOfVotesImagePath);
-            DiplaysResults();
+			InitializeComponent();
+            AddVotings();
+            ChooseVoting();
+            StackLayout stack = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { picker }
+            };
+            Content = stack;
         }
+
+        public void ChooseVoting()
+        {
+            picker = new Picker { Title = "Wybierz głosowanie:", TextColor = Color.Black, BackgroundColor = Color.Silver};
+            picker.ItemsSource = Votings;
+            picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((Picker)sender).SelectedItem.ToString() == Votings[0])
+            {
+                attendanceImagePath = "attendance.jpg";
+                GetAttendance(attendanceImagePath);
+                distributionOfVotesImagePath = "distributionOfVotes.png";
+                GetDistributionOfVotes(distributionOfVotesImagePath);
+                DiplaysResults();
+            }
+            else if (((Picker)sender).SelectedItem.ToString() == Votings[1])
+            {
+                attendanceImagePath = "attendance2.png";
+                GetAttendance(attendanceImagePath);
+                distributionOfVotesImagePath = "distributionOfVotes2.jpg";
+                GetDistributionOfVotes(distributionOfVotesImagePath);
+                DiplaysResults();
+            }
+        }
+
         public void GetAttendance(string path)
         {
             attendance = new Label
@@ -57,5 +91,18 @@ namespace VotingSystem
             };
             Content = scrollView;
         }
+
+        #region Mocking data
+        public List<string> Votings { get; set; }
+        public void AddVotings()
+        {
+            List<string> tmp = new List<string>
+            {
+                "08.12.2018r",
+                "12.12.2018r"
+            };
+            Votings = tmp;
+        }
+        #endregion
     }
 }
--- VotingSystem/VotingSystem/Vote.xaml.cs
+++ VotingSystem/VotingSystem/Vote.xaml.cs
@@ -8,32 +8,73 @@ namespace VotingSystem
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Vote : ContentPage
     {
-        public List<string> Candidates { get; set; }
-        public List<string> Authorizations { get; set; }
-
         private string authorization;
-        private Picker picker;
+        private Picker votingPicker;
+        private Picker candidatesPicker;
         private Label choice;
 
         public Vote(string token)
         {
             InitializeComponent();
-            AddCandidates();
-            AddAuthorizations();
-            AddLayout();
-            authorization = token;
-            if (!Authorizations.Contains(authorization))
+            AddVotings();
+            ChooseVoting();
+            StackLayout stack = new StackLayout()
             {
-                picker.IsEnabled = false;
-                choice.Text = "Już oddałeś swój głos.";
+                Orientation = StackOrientation.Vertical,
+                Children = { votingPicker }
+            };
+            Content = stack;
+            authorization = token;
+        }
+        public void ChooseVoting()
+        {
+            votingPicker = new Picker { Title = "Wybierz głosowanie:", TextColor = Color.Black, BackgroundColor = Color.Silver };
+            votingPicker.ItemsSource = Votings;
+            votingPicker.SelectedIndexChanged += VotingPicker_SelectedIndexChanged;
+        }
+        private void VotingPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((Picker)sender).SelectedItem.ToString() == Votings[0])
+            {
+                List<string> tmp = new List<string>
+                {
+                    "1. Jan Kowalski",
+                    "2. Mariusz Nowak",
+                    "3. Dariusz Flisak",
+                    "4. Notariusz Waluta"
+                };
+                Candidates = tmp;
+                List<string> tmp2 = new List<string>
+                {
+                    "1a",
+                    "2a"
+                };
+                Authorizations = tmp2;
+                AddLayout();
+            }
+            else if (((Picker)sender).SelectedItem.ToString() == Votings[1])
+            {
+                List<string> tmp = new List<string>
+                {
+                    "1. Marcin Kowalik",
+                    "2. Mirosław Janys",
+                    "3. Dariusz Flisak"
+                };
+                Candidates = tmp;
+                List<string> tmp2 = new List<string>
+                {
+                    "1b",
+                    "2b"
+                };
+                Authorizations = tmp2;
+                AddLayout();
             }
         }
-
         public void AddLayout()
         {
-            picker = new Picker { Title = "Select a candidate", TextColor = Color.Black};
-            picker.ItemsSource = Candidates;
-            picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+            candidatesPicker = new Picker { Title = "Select a candidate", TextColor = Color.Black };
+            candidatesPicker.ItemsSource = Candidates;
+            candidatesPicker.SelectedIndexChanged += Picker_SelectedIndexChanged;
             choice = new Label
             {
                 Text = "Proszę wybrać kandydata.",
@@ -52,16 +93,21 @@ namespace VotingSystem
             StackLayout stack = new StackLayout()
             {
                 Orientation = StackOrientation.Vertical,
-                Children = { choice, picker, goToResults }
+                Children = { choice, candidatesPicker, goToResults }
             };
             this.Content = stack;
+            if (!Authorizations.Contains(authorization))
+            {
+                candidatesPicker.IsEnabled = false;
+                choice.Text = "Już oddałeś swój głos.";
+            }
         }
         private async void Picker_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (((Picker)sender).SelectedItem.ToString() == string.Empty)
                 return;
             string candidat = ((Picker)sender).SelectedItem.ToString();
-            if(Authorizations.Contains(authorization))
+            if (Authorizations.Contains(authorization))
             {
                 var answer = await DisplayAlert("Potwierdzenie", "Czy na pewno chcesz oddać głos na: " + candidat + "?", "Tak", "Nie");
                 if (answer)
@@ -73,7 +119,6 @@ namespace VotingSystem
                 }
                 else
                     ((Picker)sender).SelectedItem = string.Empty;
-                    //((Picker)sender).SelectedItem = -1;
             }
         }
 
@@ -99,25 +144,17 @@ namespace VotingSystem
         }
 
         #region Mocking data
-        public void AddCandidates()
+        public List<string> Votings { get; set; }
+        public List<string> Candidates { get; set; }
+        public List<string> Authorizations { get; set; }
+        public void AddVotings()
         {
             List<string> tmp = new List<string>
             {
-                "1. Jan Kowalski",
-                "2. Mariusz Nowak",
-                "3. Dariusz Flisak",
-                "4. Notariusz Waluta"
+                "08.12.2018r",
+                "12.12.2018r"
             };
-            Candidates = tmp;
-        }
-        public void AddAuthorizations()
-        {
-            List<string> tmp = new List<string>
-            {
-                "1a",
-                "2a"
-            };
-            Authorizations = tmp;
+            Votings = tmp;
         }
         #endregion
     }
